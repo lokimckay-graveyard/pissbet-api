45583ab537eb1f0d8dd76bc10611d8655968fc7a
sneyed <sneyed@gmail.com>
2020-12-15 18:57:10 +1000

    Untangle path from dbPath

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import db from "./db";
 
 // CLI args
 const argv = process.argv.slice(2);
-const { port = 4000, path, d: devMode = false, p: playground = false } = mri(
-  argv
-);
-const dbPath = path || (devMode ? "pissmas-api.db" : "pissmas.db");
+const {
+  port = 4000,
+  path = "/graphql",
+  d: devMode = false,
+  p: playground = false,
+} = mri(argv);
+const dbPath = devMode ? "pissmas-api.db" : "pissmas.db";
 greet({ port, path: dbPath, playground, devMode });
 
 // Runtime
 const dbInstance = db({ devMode, dbPath });
-server({ port, dbInstance, playground });
+server({ port, path, dbInstance, playground });
