095036518bec610a927a5bdaf5643a1eed78404d
sneyed <sneyed@gmail.com>
2020-12-15 18:59:30 +1000

    Untangle path from dbPath

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const {
   d: devMode = false,
   p: playground = false,
 } = mri(argv);
-const dbPath = devMode ? "pissmas-api.db" : "pissmas.db";
-greet({ port, path: dbPath, playground, devMode });
+greet({ port, path, playground, devMode });
 
 // Runtime
+const dbPath = devMode ? "pissmas-api.db" : "pissmas.db";
 const dbInstance = db({ devMode, dbPath });
 server({ port, path, dbInstance, playground });
